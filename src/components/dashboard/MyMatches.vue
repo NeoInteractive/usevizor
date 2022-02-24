210f04b1de1861deed8421faca21bb0a914a6d55
Eric Kelley <hello@ericmkelley.com>
2022-02-24 14:37:11 -0500

    separate matches into categories

--- src/components/dashboard/MyMatches.vue
+++ src/components/dashboard/MyMatches.vue
@@ -32,25 +32,70 @@ onMounted(() => {
 
 <template>
   <div>
-    <div class="flex flex-row justify-between mb-8 items-center">
-      <h1 class="">My Matches</h1>
+    <div
+      class="flex flex-row justify-between mb-8 items-center bg-gray-950 p-4 border-b border-r border-gray-600 bg-gradient-to-r from-gray-900 via-gray-925 to-gray-950"
+    >
+      <h1 class="text-white tracking-wide font-normal text-xl">My Matches</h1>
       <div>
-        <code class="mr-6">{{ matches.length }} / 5 Matches Created</code>
+        <code class="">{{ matches.length }} / 5 </code>
+        <span class="tooltip tooltip-top mr-6" data-tip="Free matches created">
+          <i class="fa-duotone fa-circle-question"></i>
+        </span>
         <button @click="emit('createMatch')" class="btn btn-sm">
           <i class="fa-solid fa-plus"></i>
         </button>
+        <button
+          @click="emit('createMatch')"
+          class="py-1 px-4 rounded text-red-400"
+        >
+          <i class="fa-solid fa-trash"></i>
+        </button>
       </div>
     </div>
-    <div v-if="matches.length > 0" class="flex flex-row flex-wrap">
+    <div v-if="matches.length > 0" class="flex flex-row flex-wrap px-4">
+      <h2
+        class="subheading w-full mb-6 text-sm tracking-widest text-indigo-400 font-bold uppercase"
+      >
+        Free Matches
+      </h2>
+      <router-link
+        tag="div"
+        :to="`/dashboard/m/${match.id}`"
+        v-for="match in matches"
+        :key="match.id"
+        class="sm:w-full lg:w-1/2 xl:w-1/3 p-2"
+        v-show="!match.scoreboard.premium"
+      >
+        <div
+          class="h-28 bg-gray-950 border border-gray-500 p-4 flex flex-col justify-between cursor-pointer hover:bg-gray-925 hover:border-indigo-400 transition active:bg-violet-700"
+        >
+          <div class="flex flex-row justify-between">
+            <h1 class="text-sm">{{ match.heading }}</h1>
+            <span v-if="match.scoreboard.premium" class="badge badge-secondary">
+              Pro
+            </span>
+            <span v-else class="badge">Basic</span>
+          </div>
+          <h1 class="text-xs font-light text-indigo-100 opacity-70">
+            {{ match.status }} | {{ match.id.toUpperCase() }}
+          </h1>
+        </div>
+      </router-link>
+      <h2
+        class="subheading w-full mb-6 text-sm tracking-widest text-indigo-400 font-bold uppercase mt-12"
+      >
+        Premium Matches
+      </h2>
       <router-link
         tag="div"
         :to="`/dashboard/m/${match.id}`"
         v-for="match in matches"
         :key="match.id"
         class="w-1/3 p-2"
+        v-show="match.scoreboard.premium"
       >
         <div
-          class="h-28 bg-gray-950 border border-gray-500 p-4 flex flex-col justify-between cursor-pointer hover:bg-gray-925 hover:border-indigo-400 transition rounded-md active:bg-violet-700"
+          class="h-28 bg-gray-950 border border-gray-500 p-4 flex flex-col justify-between cursor-pointer hover:bg-gray-925 hover:border-indigo-400 transition active:bg-violet-700"
         >
           <div class="flex flex-row justify-between">
             <h1 class="text-sm">{{ match.heading }}</h1>
