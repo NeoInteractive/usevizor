118037f0f13b70c72c12e6e702ebd4f4448f6489
Eric Kelley <hello@ericmkelley.com>
2022-03-10 21:37:42 -0500

    remove dupe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,31 +26,8 @@
   <main class="bg-gray-900">
     <div class="flex justify-center">
       <section
-        class="pt-48 bg-gray-925/25 w-1/3 rounded backdrop-blur-sm border border-gray-925"
-      >
-        <img src="@/assets/logo-long.svg" alt="Vizor Logo" class="w-48 mb-12" />
-        <h1 class="text-4xl mb-6">
-          Add a scoreboard to your tournament in 3 minutes.
-        </h1>
-        <p class="text-xl font-light mb-6">
-          VIZOR is a fully dynamic scoreboard that offers tournament admins
-          easy-to-use and customizable scoreboard overlays, with the ability to
-          control from a Web Browser. And soon, using an OBS Dock, or our Stream
-          Deck Plugins.
-        </p>
-        <p class="text-xl font-light mb-6">
-          We’re currently in Open Beta. If you’re interested in joining the
-          beta, register for access using the button below and create your first
-          overlay today!
-        </p>
-        <router-link
-          tag="button"
-          to="/auth/signup"
-          class="btn btn-primary tracking-widest btn-sm mt-8"
-        >
-          Create an Account
-        </router-link>
-      </section>
+        class="py-48 bg-gray-925/25 w-1/3 rounded backdrop-blur-sm border border-gray-925"
+      ></section>
     </div>
   </main>
 </template>
