befe789cd51b5a77aabdb864b6432ce4a3b5020c
Eric Kelley <hello@ericmkelley.com>
2022-03-12 19:41:15 -0500

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,79 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+import ScoreboardDefault from "../components/scoreboards/ScoreboardDefault.vue";
+let newMatch = reactive({
+  id: "000",
+  heading: "VIZOR Invitational 2022",
+  subheading: "Best of 7",
+  status: "active",
+  created_at: "9999-99-99",
+  owner: undefined,
+  team_one: {
+    score: 4,
+    logo: "https://cdn.usevizor.com/default_logo.png",
+    name: "Phoenix",
+    show_logo: true,
+    color: {
+      bg: "rgb(142, 28, 28)",
+      text: "rgb(255, 255, 255)",
+    },
+  },
+  team_two: {
+    score: 5,
+    logo: "https://cdn.usevizor.com/default_logo.png",
+    show_logo: true,
+    name: "Sentinels",
+    color: {
+      bg: "rgb(30, 61, 159)",
+      text: "rgb(255, 255, 255)",
+    },
+  },
+  scoreboard: {
+    theme: "default",
+    dark: true,
+    show_heading: true,
+    show_subheading: true,
+    premium: false,
+    sponsors: false,
+  },
+});
+let newMatch2 = reactive({
+  id: "000",
+  heading: "VIZOR Invitational 2022",
+  subheading: "Best of 7",
+  status: "active",
+  created_at: "9999-99-99",
+  owner: undefined,
+  team_one: {
+    score: 4,
+    logo: "https://cdn.usevizor.com/default_logo.png",
+    name: "Phoenix",
+    show_logo: true,
+    color: {
+      bg: "rgb(142, 28, 28)",
+      text: "rgb(255, 255, 255)",
+    },
+  },
+  team_two: {
+    score: 5,
+    logo: "https://cdn.usevizor.com/default_logo.png",
+    show_logo: true,
+    name: "Sentinels",
+    color: {
+      bg: "rgb(30, 61, 159)",
+      text: "rgb(255, 255, 255)",
+    },
+  },
+  scoreboard: {
+    theme: "default",
+    dark: false,
+    show_heading: true,
+    show_subheading: true,
+    premium: false,
+    sponsors: false,
+  },
+});
+</script>
 <template>
   <section id="hero" class="bg-indigo-900 py-48 flex justify-center">
     <div class="flex justify-between w-3/4 max-w-[100rem]">
@@ -17,19 +92,22 @@
         </router-link>
       </div>
 
-      <div
-        id="hero-demo"
-        class="bg-gray-900 w-1/2 aspect-[21/9] p-4 -skew-y-6 transform-gpu rounded-xl shadow-xl"
-      ></div>
+      <div class="w-1/2 flex flex-col justify-center">
+        <scoreboard-default :localMatchData="newMatch" />
+        <br />
+        <scoreboard-default :localMatchData="newMatch2" />
+      </div>
     </div>
   </section>
-  <main class="bg-gray-900">
-    <div class="flex justify-center">
-      <section
-        class="py-48 bg-gray-925/25 w-1/3 rounded backdrop-blur-sm border border-gray-925"
-      ></section>
+  <section class="bg-gray-925 py-48 flex justify-center">
+    <div class="flex justify-between w-3/4 max-w-[100rem]">
+      <div class="w-full pr-8">
+        <h1 class="text-white text-2xl text-center pr-3">
+          VIZOR is currently in Open Beta.
+        </h1>
+      </div>
     </div>
-  </main>
+  </section>
 </template>
 
 <style lang="postcss" scoped>
